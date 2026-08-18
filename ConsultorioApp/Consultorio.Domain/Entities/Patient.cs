17e57e2fec7c12460825f74638acff2633cf519c
Dan-Dude-UK <ezequielareche9@gmail.com>
2026-08-18 01:21:59 -0400

    Add files via upload

---
+++ ConsultorioApp/Consultorio.Domain/Entities/Patient.cs
@@ -0,0 +1,21 @@
+using Consultorio.Domain.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Consultorio.Infrastructura.Models
+{
+    // Paciente
+    public class Patient : BasePerson
+    {
+        public DateTime RegistrationDate { get; set; }
+
+        public string BloodType { get; set; } = string.Empty;
+
+        public string MedicalHistory { get; set; } = string.Empty;
+
+        public bool Active { get; set; }
+    }
+}
